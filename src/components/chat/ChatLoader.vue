13a35f9f671f79b0e3aab8bd27b9fc1253dc55f1
이준용 <iiiuooyoo@gmail.com>
2023-11-03 17:24:18 +0900

    feat : 채팅 로딩 컴포넌트 추가

---
+++ src/components/chat/ChatLoader.vue
@@ -0,0 +1,25 @@
+<template>
+    <li class="justify-center !pl-0">
+        <span class="w-[30px] h-[30px] rounded-[50%] relative"></span>
+    </li>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+li {
+    span {
+        animation: rotate 1s linear infinite;
+
+        &::before {
+            content: "";
+            box-sizing: border-box;
+            position: absolute;
+            inset: 0px;
+            border-radius: 50%;
+            border: 3px solid rgb(222, 225, 229);
+            animation: prixClipFix 2s linear infinite;
+        }
+    }
+}
+</style>
--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -4,5 +4,14 @@ import ChatContainer from "./ChatContainer.vue";
 import ChatList from "./ChatList.vue";
 import ChatItem from "./ChatItem.vue";
 import ChatInput from "./ChatInput.vue";
+import ChatLoader from "./ChatLoader.vue";
 
-export { Chat, ChatButton, ChatContainer, ChatList, ChatItem, ChatInput };
+export {
+    Chat,
+    ChatButton,
+    ChatContainer,
+    ChatList,
+    ChatItem,
+    ChatInput,
+    ChatLoader,
+};
