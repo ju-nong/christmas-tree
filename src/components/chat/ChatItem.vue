08d6946ac929cd28cae7a10fe1570189b02e7262
이준용 <iiiuooyoo@gmail.com>
2023-11-03 15:03:31 +0900

    feat : 채팅 아이템 추가

--- src/components/chat/ChatItem.vue
+++ src/components/chat/ChatItem.vue
@@ -1,7 +1,57 @@
 <template>
-    <li></li>
+    <li :class="[{ first: showNickname }, { mine: isMine }]">
+        <div v-if="showNickname" class="nickname">
+            <span>{{ chat.nickname }}</span>
+        </div>
+        <p>{{ chat.text }}</p>
+        <span v-if="showDate">{{ formatCreateAt }}</span>
+    </li>
 </template>
 
-<script setup></script>
+<script setup>
+import { defineProps, toRefs } from "vue";
+
+import { useUser } from "../../stores";
+
+import dayjs from "dayjs";
+
+const props = defineProps({
+    chat: {
+        type: Object,
+        required: true,
+    },
+    beforeChat: {
+        type: Object,
+        required: false,
+    },
+    afterChat: {
+        type: Object,
+        required: false,
+    },
+});
+
+const { chat, beforeChat, afterChat } = toRefs(props);
+
+const user = useUser();
+
+// date formatting
+const formatDate = (timestamp) => dayjs(timestamp.toDate()).format("A h:mm");
+
+// 등록일
+const formatCreateAt = formatDate(chat.value.createAt);
+
+// 닉네임 보여줄지
+const showNickname =
+    beforeChat.value === null ||
+    chat.value.nickname !== beforeChat.value.nickname;
+
+// 시간 보여줄지
+const showDate =
+    afterChat.value === null ||
+    formatCreateAt !== formatDate(afterChat.value.createAt);
+
+// 내 채팅인지
+const isMine = chat.value.nickname === user.nickname;
+</script>
 
 <style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import "./style.scss";
 import { db } from "./firebase";
 import { createPinia } from "pinia";
 
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+
+dayjs.locale("ko");
+
 console.log(db);
 
 createApp(App).use(createPinia()).mount("#app");
